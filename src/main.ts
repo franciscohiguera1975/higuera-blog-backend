bcc7990829f2ee9029de99f288ff0ee724a76efa
Prof. Francisco Higuera <prof.francisco.higuera@gmail.com>
2026-06-22 06:37:46 -0500

    feat: added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express'; // 👈 Importación necesaria
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); // 👈 Tipo específico
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
   app.useStaticAssets(join(__dirname, '..', 'public')); // 👈 Habilita acceso público a /public
+
+  const config = new DocumentBuilder()
+    .setTitle('Mi API')
+    .setDescription('Documentación de la API')
+    .setVersion('1.0')
+    .addBearerAuth() // opcional si usas JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
